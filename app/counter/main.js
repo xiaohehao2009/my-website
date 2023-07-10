31d3a4180e21ae463b3c54045e94e06330fddb02
xiaohehao2009 <1607509493@qq.com>
2023-07-10 18:33:35 +0800

    fix bug: counter, to prevent count from stopping increasing after reaching 9007199254740991

--- app/counter/main.js
+++ app/counter/main.js
@@ -4,42 +4,49 @@ import { useState, useEffect } from 'react';
 import { parse } from 'qs';
 import { usePathname, useRouter} from 'next/navigation';
 
+const convert = str => {
+    const index = str.indexOf('.');
+    return index === -1 ? BigInt(str) : BigInt(str.slice(0, index));
+};
+
 export default function Component() {
     const [count, setCount] = useState(null);
     const router = useRouter();
     const pathname = usePathname();
     useEffect(() => {
-        let c = +localStorage.getItem('count') || 0;
+        let c = convert(localStorage.getItem('count') || '0');
         const s = location.search.slice(1);
         if (s.length) {
             const obj = parse(s);
             if (Object.hasOwn(obj, 'set')) {
-                c = +obj.set;
+                c = convert(obj.set);
             }
             if (Object.hasOwn(obj, 'add')) {
-                c += +obj.add;
+                c += convert(obj.add);
             }
             if (Object.hasOwn(obj, 'mul')) {
-                c *= obj.mul;
+                c *= convert(obj.mul);
             }
             if (Object.hasOwn(obj, 'div')) {
-                c /= obj.div;
+                c /= convert(obj.div);
             }
         }
         setCount(c);
         if (s.length) router.replace(pathname);
     }, [pathname]);
     useEffect(() => {
-        if (count !== null) localStorage.setItem('count', String(count));
+        if (count !== null) {
+            localStorage.setItem('count', count.toString());
+        }
     }, [count]);
 
     return (
         <>
             <h1>计数器</h1>
             <hr />
-            <p>计数: {count}</p>
+            <p>计数: {count?.toString()}</p>
             <button
-                onClick={() => setCount(count + 1)}
+                onClick={() => setCount(c => c + 1n)}
                 style={{
                     width: '100px',
                     height: '100px',
